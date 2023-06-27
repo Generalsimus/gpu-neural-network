fd48a96cb5f1330f5d4a953735a19894793c8d2e
Generalsimus <totorotomoney@gmail.com>
2023-06-28 01:46:19 +0400

    update

--- main.go
+++ main.go
@@ -1,29 +1,48 @@
 package main
 
-/*
-#include <stdlib.h>
-#include "./test.h"
-*/
-import "C"
+// #include <stdio.h>
+// #include "./test.h"
 import (
-	"fmt"
-	"unsafe"
+	"C"
 )
 
 func main() {
-	firsName := C.CString("sssa")
+	// size := 5
+	// Allocate memory for the int array
+	// array := (*C.int)(C.malloc(C.size_t(size) * C.sizeof_int))
 
-	lastName := C.CString("ss22s")
+	// defer C.free(unsafe.Pointer(array)) // Deallocate memory when done
+	// fmt.Println([]C.int{-1, 2, 4, 0, 5, 3, 6, 2, 1})
 
-	age := C.int(21)
-	msdk2 := C.struct_person_t{
-		firstName: firsName,
-		lastName:  lastName,
-		age:       age,
-	}
-	defer C.free(unsafe.Pointer(firsName))
-	defer C.free(unsafe.Pointer(lastName))
-	// defer C.free(unsafe.Pointer(age))
-	fmt.Println("I am in Go code now!", msdk2, msdk2.person_new(), firsName)
+	// arr := []C.int{1, 2, 3, 4, 5}
+	// size := C.int(len(arr))
+
+	// C.CreateModel(&arr[0], size)
+	// C.CreateModel([]C.int{-1, 2, 4, 0, 5, 3, 6, 2, 1})
+	// C.printIntArray([]C.int{-1, 2, 4, 0, 5, 3, 6, 2, 1})
+	// // Access and modify the array elements
+	// for i := 0; i < size; i++ {
+	// 	array[i] = C.int(i + 1)
+	// }
+
+	// C.CreateModel(&[]C.int{3, 2, 1})
+	C.helloFromC()
+
+	// fff := C.CreateModel([]C.int{3, 2, 1})
+	// fmt.Println("a ...any: ", fff)
+	// firsName := C.CString("sssa")
+
+	// lastName := C.CString("ss22s")
+
+	// age := C.int(21)
+	// msdk2 := C.struct_person_t{
+	// 	firstName: firsName,
+	// 	lastName:  lastName,
+	// 	age:       age,
+	// }
+	// defer C.free(unsafe.Pointer(firsName))
+	// defer C.free(unsafe.Pointer(lastName))
+	// // defer C.free(unsafe.Pointer(age))
+	// fmt.Println("I am in Go code now!", msdk2, msdk2.person_new(), firsName)
 	// C.inC()
 }
--- test.h
+++ test.h
@@ -1,10 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <cuda_runtime.h>
+// #include <cuda_runtime.h>
+typedef struct Model
+{
+    int *layersSizes;
+    int count;
+} Model;
 
-typedef struct person_t {
-  char* firstName;
-  char* lastName;
-  int  age;
-} person_t;
+// typedef struct Layer
+// {
+//     int *layersSizes;
+//     int count;
+// } Layer;
 
-person_t* person_new() {
-    return (person_t*)malloc(sizeof(person_t));
-} 
+void CreateWidth(int size)
+{
+    float *inputs = (float *)malloc(size * sizeof(float));
+
+    float *d_inputs;
+
+    cudaMalloc((void **)&d_inputs, size * sizeof(float));
+
+    cudaMemcpy(d_inputs, inputs, size * sizeof(float), cudaMemcpyHostToDevice);
+
+    printf("inputs: %d\n", inputs);
+    printf("d_inputs: %d\n", d_inputs);
+
+    free(inputs);
+    cudaFree(d_inputs);
+}
+
+void CreateModel(Model model)
+{
+    int layersCount = model.count;
+    int *layersSizes = model.layersSizes;
+    int widthCount = 0;
+    int prevValue = 0;
+    for (int i = 0; i < layersCount; i++)
+    {
+        int layerInputsSize = layersSizes[i];
+        widthCount = widthCount + (layerInputsSize * prevValue);
+
+        printf("Element %d: %d\n", i, layerInputsSize);
+        prevValue = layerInputsSize;
+    }
+    printf("eeee: %d\n", widthCount);
+    CreateWidth(widthCount);
+    // widths :=
+}
+
+// extern "C"
+// {
+    void helloFromC()
+    {
+        int sizes[] = {3, 5, 2};
+        Model model = {
+            sizes,
+            sizeof(sizes) / sizeof(sizes[0]),
+        };
+
+        CreateModel(model);
+        printf("Eleqweqwement: %d\n", model);
+    }
+// }
